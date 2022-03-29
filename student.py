4ae934ad9e8b08b270292ddebbe6ed645c625cb9
zachli359 <81203486+zachli359@users.noreply.github.com>
2022-03-29 09:05:25 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,17 @@
 def main():
-    #your code goes here
-    print("0")
+  #your code goes here
+  number = int(input("Enter a number\n"))
 
+  if (number % 3 == 0) and (number % 5 == 0):
+    print("fizzbuzz")
+
+  elif (number % 3) == 0:
+    print("fizz")
+
+  elif (number % 5) == 0:
+    print("buzz")
+
+  else:
+    print(number)
 if __name__ =='__main__':
     main()
